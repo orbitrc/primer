7a24aa4c17459819a4bbb383309d9a4a7ef32490
Yujŏnja <hardboiled65@gmail.com>
2021-10-18 07:19:29 +0900

    Mutability test.

--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,10 @@ mod tests {
         let mut map: Map = Map::new();
         map.insert(String::from("Foo"),
             JsonValue::Number(JsonNumber::from_f64(42.0).unwrap()));
+        map["Foo"] = json!(42);
         let object = JsonValue::Object(map);
         let j = json!({
-            "Foo": 42.0,
+            "Foo": 42,
         });
         assert_eq!(object, j);
     }
