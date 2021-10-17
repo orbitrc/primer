d90db5edfd1b3d5a625083d830f6962198a6108a
Yujŏnja <hardboiled65@gmail.com>
2021-10-18 06:38:50 +0900

    Add more test.

--- src/json.rs
+++ src/json.rs
@@ -1,3 +1,6 @@
 use serde_json;
 
 pub type JsonValue = serde_json::Value;
+pub type Map = serde_json::map::Map<String, JsonValue>;
+pub type Number = serde_json::Number;
+pub use serde_json::json;
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,24 @@ pub mod random;
 #[cfg(test)]
 mod tests {
     use super::json::JsonValue;
+    use super::json::Map;
+    use super::json::Number;
+    use super::json::json;
 
     #[test]
     fn json_value() {
         let null = JsonValue::Null;
         assert_eq!(null, JsonValue::Null);
+    }
+
+    #[test]
+    fn json_object() {
+        let mut map: Map = Map::new();
+        map.insert(String::from("Foo"), JsonValue::Number(Number::from_f64(42.0).unwrap()));
+        let object = JsonValue::Object(map);
+        let j = json!({
+            "Foo": 42.0,
+        });
+        assert_eq!(object, j);
     }
 }
