319f13159cb71f035a455f6e1d7b021771823d7d
Yujŏnja <hardboiled65@gmail.com>
2021-10-25 06:10:48 +0900

    Random test.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ mod tests {
     use super::json::JsonNumber;
     use super::json::json;
 
+    use super::random::Random;
+
     #[test]
     fn json_value() {
         let null = JsonValue::Null;
@@ -25,5 +27,12 @@ mod tests {
             "Foo": 42,
         });
         assert_eq!(object, j);
+    }
+
+    #[test]
+    fn rand_not_nan_or_infinity() {
+        let random_number = f64::random(0.0..1.0);
+        assert_eq!(random_number, f64::NAN);
+        assert_eq!(random_number, f64::INFINITY);
     }
 }
--- src/random.rs
+++ src/random.rs
@@ -4,65 +4,65 @@ use rand::{Rng, thread_rng};
 
 /// Random trait provides random method to numeric types.
 pub trait Random {
-    fn random(&self, range: Range<Self>) -> Self where Self: Sized;
+    fn random(range: Range<Self>) -> Self where Self: Sized;
 }
 
 impl Random for i8 {
-    fn random(&self, range: Range<i8>) -> i8 {
+    fn random(range: Range<i8>) -> i8 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for u8 {
-    fn random(&self, range: Range<u8>) -> u8 {
+    fn random(range: Range<u8>) -> u8 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for i16 {
-    fn random(&self, range: Range<i16>) -> i16 {
+    fn random(range: Range<i16>) -> i16 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for u16 {
-    fn random(&self, range: Range<u16>) -> u16 {
+    fn random(range: Range<u16>) -> u16 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for i32 {
-    fn random(&self, range: Range<i32>) -> i32 {
+    fn random(range: Range<i32>) -> i32 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for u32 {
-    fn random(&self, range: Range<u32>) -> u32 {
+    fn random(range: Range<u32>) -> u32 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for i64 {
-    fn random(&self, range: Range<i64>) -> i64 {
+    fn random(range: Range<i64>) -> i64 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for u64 {
-    fn random(&self, range: Range<u64>) -> u64 {
+    fn random(range: Range<u64>) -> u64 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for f32 {
-    fn random(&self, range: Range<f32>) -> f32 {
+    fn random(range: Range<f32>) -> f32 {
         thread_rng().gen_range(range)
     }
 }
 
 impl Random for f64 {
-    fn random(&self, range: Range<f64>) -> f64 {
+    fn random(range: Range<f64>) -> f64 {
         thread_rng().gen_range(range)
     }
 }
