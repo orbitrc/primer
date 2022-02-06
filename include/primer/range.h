366923a7fe35de44c47e38bd2e6f10860d325854
Yujŏnja <hardboiled65@gmail.com>
2022-02-07 08:50:34 +0900

    C++ APIs for Range.

--- include/primer/range.h
+++ include/primer/range.h
@@ -18,15 +18,40 @@ public:
 
     Range<T>(T start, T end, bool exclusive=true);
 
-    ~Range();
+    ~Range()
+    {
+        pr_range_free(this->range);
+    }
 
     //=========================
     // Element access
     //=========================
 
-    T start() const;
+    T start() const
+    {
+        return this->range.start;
+    }
 
-    T end() const;
+    T end() const
+    {
+        return this->range.end;
+    }
+
+    bool exclusive() const
+    {
+        return this->range.exclusive;
+    }
+
+    //=========================
+    // Others
+    //=========================
+
+    bool contains(const T value) const
+    {
+        return pr_range_contains(this->range, static_cast<int64_t>(value));
+    }
+
+    T random() const;
 };
 
 } // namespace pr
--- src/range.cpp
+++ src/range.cpp
@@ -6,6 +6,10 @@
 
 namespace pr {
 
+//================
+// Constructor
+//================
+
 // int8_t
 template<>
 Range<int8_t>::Range(int8_t start, int8_t end, bool exclusive)
@@ -34,10 +38,92 @@ Range<uint16_t>::Range(uint16_t start, uint16_t end, bool exclusive)
     this->range = pr_range_new(start, end, exclusive);
 }
 
-template <typename T>
-Range<T>::~Range()
+// int32_t
+template<>
+Range<int32_t>::Range(int32_t start, int32_t end, bool exclusive)
 {
-    pr_range_free(this->range);
+    this->range = pr_range_new(start, end, exclusive);
+}
+
+// uint32_t
+template<>
+Range<uint32_t>::Range(uint32_t start, uint32_t end, bool exclusive)
+{
+    this->range = pr_range_new(start, end, exclusive);
+}
+
+// int64_t
+template<>
+Range<int64_t>::Range(int64_t start, int64_t end, bool exclusive)
+{
+    this->range = pr_range_new(start, end, exclusive);
+}
+
+// uint64_t
+template<>
+Range<uint64_t>::Range(uint64_t start, uint64_t end, bool exclusive)
+{
+    this->range = pr_range_new(start, end, exclusive);
+}
+
+//==============
+// Random
+//==============
+
+// int8_t
+template<>
+int8_t Range<int8_t>::random() const
+{
+    return pr_range_random(this->range);
+}
+
+// uint8_t
+template<>
+uint8_t Range<uint8_t>::random() const
+{
+    return pr_range_random_u(this->range);
+}
+
+// int16_t
+template<>
+int16_t Range<int16_t>::random() const
+{
+    return pr_range_random(this->range);
+}
+
+// uint16_t
+template<>
+uint16_t Range<uint16_t>::random() const
+{
+    return pr_range_random_u(this->range);
+}
+
+// int32_t
+template<>
+int32_t Range<int32_t>::random() const
+{
+    return pr_range_random(this->range);
+}
+
+// uint32_t
+template<>
+uint32_t Range<uint32_t>::random() const
+{
+    return pr_range_random_u(this->range);
+}
+
+// int64_t
+template<>
+int64_t Range<int64_t>::random() const
+{
+    return pr_range_random(this->range);
+}
+
+// uint64_t
+template<>
+uint64_t Range<uint64_t>::random() const
+{
+    return pr_range_random_u(this->range);
 }
 
 } // namespace pr
